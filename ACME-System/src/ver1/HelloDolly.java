785cbdc09557ea32d6a6f4b887b8314821c8e28c
August West <aug1west@gmail.com>
2022-08-27 13:45:13 -0400

    more dolly stuff

--- ACME-System/src/ver1/HelloDolly.java
+++ ACME-System/src/ver1/HelloDolly.java
@@ -4,6 +4,7 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		System.out.println("Person 2 is at work!!");
+		System.out.println("Person 2 is at work with more stuff!");
 
 	}
 
